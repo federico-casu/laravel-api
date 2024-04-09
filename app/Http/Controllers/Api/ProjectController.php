697afc1ec8575f3d4843d29977f6fe20b5a8c5b3
federico-casu <federico.casu1101@gmail.com>
2024-04-09 17:48:05 +0200

    feat: added paginate method to db call to get just 3 projects for each page; edit to db call to get projects with type and technologies references

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,7 +10,9 @@ class ProjectController extends Controller
 {
     public function index() {
 
-        $projects = Project::all();
+        // $projects = Project::all();
+
+        $projects = Project::with('type', 'technologies')->paginate(3);
 
         return response()->json([
             'success' => true,
