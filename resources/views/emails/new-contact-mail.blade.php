e96cc5e1096747eba2749825a5868e37311448ff
federico-casu <federico.casu1101@gmail.com>
2024-04-12 18:56:25 +0200

    feat: added NewContact class; added Lead model, leads table and LeadController to manage the sending of emails

---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,9 @@
+<h1>Ciao Admin</h1>
+
+<p>
+    Hai ricevuto un nuovo messaggio, ecco i dettagli:<br>
+    Nome: {{ $lead->name }}<br>
+    Email: {{ $lead->email }}<br>
+    Message:<br>
+    {{ $lead->message }}
+</p>
